6b5e433fcbe288c5d10271e74a36c67f573355cd
tobias <tobirastorza@gmail.com>
2023-04-24 00:29:09 -0300

    24 added eslint

--- index.js
+++ index.js
@@ -9,96 +9,96 @@ app.use(express.static("build"));
 app.use(morgan("tiny"));
 
 app.get("/api/persons", (request, response) => {
-  try {
-    Person.find({}).then((person) => {
-      response.json(person);
-    });
-  } catch (e) {
-    response.status(500).send("Error retrieving persons");
-  }
+    try {
+        Person.find({}).then((person) => {
+            response.json(person);
+        });
+    } catch (e) {
+        response.status(500).send("Error retrieving persons");
+    }
 });
 
 app.get("/info", (request, response) => {
-  try {
-    Person.countDocuments().then((count) => {
-      response.send(`<p>Phonebook has info for ${count} people</p>`);
-    });
-  } catch (e) {
-    response.status(500).send("Error retrieving person count");
-  }
+    try {
+        Person.countDocuments().then((count) => {
+            response.send(`<p>Phonebook has info for ${count} people</p>`);
+        });
+    } catch (e) {
+        response.status(500).send("Error retrieving person count");
+    }
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  try {
-    Person.findById(request.params.id).then((person) => {
-      response.json(person);
-    });
-  } catch (e) {
-    response.status(500).send("Error retrieving person");
-  }
+    try {
+        Person.findById(request.params.id).then((person) => {
+            response.json(person);
+        });
+    } catch (e) {
+        response.status(500).send("Error retrieving person");
+    }
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
-  try {
-    const { id } = request.params;
-    Person.findByIdAndRemove(id).then(
-      response.status(204).send("Person deleted")
-    );
-  } catch (e) {
-    response.status(404).send(e.message);
-  }
+    try {
+        const { id } = request.params;
+        Person.findByIdAndRemove(id).then(
+            response.status(204).send("Person deleted")
+        );
+    } catch (e) {
+        response.status(404).send(e.message);
+    }
 });
 
 app.post("/api/persons", async (request, response, next) => {
-  const person = {
-    name: request.body.name,
-    number: request.body.number,
-  };
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+    };
 
-  const newPerson = new Person(person);
+    const newPerson = new Person(person);
 
-  try {
-    const existingPerson = await Person.findOne({ name: person.name });
+    try {
+        const existingPerson = await Person.findOne({ name: person.name });
 
-    if (existingPerson) {
-      const updatedPerson = await Person.findOneAndUpdate(
-        { name: person.name },
-        { number: person.number },
-        { new: true }
-      );
+        if (existingPerson) {
+            const updatedPerson = await Person.findOneAndUpdate(
+                { name: person.name },
+                { number: person.number },
+                { new: true }
+            );
 
-      console.log("updated");
-      response.json(updatedPerson);
-    } else {
-      const savedPerson = await newPerson.save();
-      console.log("saved");
-      response.json(savedPerson);
+            console.log("updated");
+            response.json(updatedPerson);
+        } else {
+            const savedPerson = await newPerson.save();
+            console.log("saved");
+            response.json(savedPerson);
+        }
+    } catch (e) {
+        response.status(404).send("error validating");
     }
-  } catch (e) {
-    response.status(404).send("error validating");
-  }
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+    const body = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
 
-  Person.findOneAndUpdate(
-    { name: person.name },
-    { number: person.number },
-    { new: true }
-  )
-    .then((updatedPerson) => {
-      response.json(updatedPerson);
-    })
-    .catch((error) => next(error));
+    Person.findOneAndUpdate(
+        { name: person.name },
+        { number: person.number },
+        { new: true }
+    )
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
